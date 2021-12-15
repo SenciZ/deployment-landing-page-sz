248eddc11fb04aee175b786393e314f85b09e164
Senad Zenkovic <zenkovic88@gmail.com>
2021-12-15 13:53:01 -0700

    create styling

--- server/index.js
+++ server/index.js
@@ -11,6 +11,10 @@ app.get('/css', (req, res)=>{
     res.sendFile(path.join(__dirname, '../style.css'))
 })
 
+app.get('/js', (req, res)=>{
+    res.sendFile(path.join(__dirname, '../main.js'))
+})
+
 const port = process.env.PORT || 4005
 
 app.listen(port, ()=>{console.log(`Listenting on port ${port}`)})
